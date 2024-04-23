e30adec798c8aaf28c8a36e8286e417c9aa2b007
LAPTOP-1Q0NKG6M\Serge <Sergey2018sar@yandex.ru>
2024-04-23 19:29:33 +0400

    update project

--- main.py
+++ main.py
@@ -1,15 +1,15 @@
 from flask import Flask
 
 from failsafe_cron.models.info_data import InfoData
-from failsafe_cron.services.scheduled_service import ScheduledService
+from failsafe_cron.services.redis_lock_service import RedisLockService
 
 app = Flask(__name__)
 
-scheduled_service = ScheduledService()
+redis_lock_service = RedisLockService()
 
 def run_script():
     new_data = InfoData(name='Name', description='Description', date_created='2024-04-20')
-    scheduled_service.script(new_data)
+    redis_lock_service.script(new_data)
 
 if __name__ == '__main__':
     run_script()
